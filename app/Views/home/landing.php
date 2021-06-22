922497b4110c33e68b58d2842e36c687d06bd524
Dicki <dickiclever@gmail.com>
2021-06-22 16:52:41 +0700

    edit landing page view

--- app/Views/home/landing.php
+++ app/Views/home/landing.php
@@ -1,5 +1,24 @@
 <?= $this->extend('layouts/template.php') ?>
 
 <?= $this->section('content') ?>
-
+<div class="main">
+    <div class="jumbotron">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="jumbotron_landingPage_left">
+                        <h1 class="jumbotron_landingPage_h1">Beli Peliharaan dan Kebutuhannya Jadi Mudah</h1>
+                        <p>Dengan PupHub, Kamu bisa mencari hewan peliharaan dan kebutuhannya dengan mudah, cepat, dan aman. Yuk belanja sekarang!</p>
+                        <button type="button" class="btn jumbotron_landingPage_btn">Mulai Belanja</button>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="jumbotron_landingPage_right">
+                        <img src="<?= base_url("images/gugukLandingPage.png") ?>" alt="gugukLandingPage" class="jumbotron_landingPage_imageRight">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?= $this->endsection() ?>
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -1,6 +1,8 @@
 <div class="header">
   <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="#">PupHub</a>
+    <a class="navbar-brand" href="#">
+      <img src="<?= base_url("images/logo.png") ?>" alt="PubHub">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
