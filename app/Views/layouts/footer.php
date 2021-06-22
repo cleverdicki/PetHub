7a06b762078755b627222012a16b1ceac0bcab1a
Dicki <dickiclever@gmail.com>
2021-06-22 09:05:22 +0700

    add layout for header and footer

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,32 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    testing
+                                </div>
+                                <div class="col-md-6">
+                                    broo
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <h6 class="h6_footer_left_title">Disponsori oleh: BEM ITS</h6>
+                        <h6 class="h6_footer_left">Ditmawa ITS</h6>
+                        <h6 class="h6_footer_left">Kelompok A08 MPPL</h6>
+                    </div>
+                    <div class="col-md-4">
+                        <h6 class="h6_footer_right_title">Disponsori oleh: BEM ITS</h6>
+                        <h6 class="h6_footer_right">Ditmawa ITS</h6>
+                        <h6 class="h6_footer_right">Kelompok A08 MPPL</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ app/Views/layouts/header.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">PetHub</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Hewan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Perlengkapan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Tentang</a>
+      </li>
+    </ul>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Daftar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
